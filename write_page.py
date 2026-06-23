aecce851dfd9963343fd949ebad6d05c804e576f
Daryo <djmirontos@gmail.com>
2026-06-23 10:36:10 +0300

    chore: remove leftover temp script

--- write_page.py
+++
@@ -1,3 +0,0 @@
-content = open('C:/Users/bridge/docux-web/write_page_content.txt', encoding='utf-8').read()
-open('C:/Users/bridge/docux-web/src/app/page.tsx', 'w', encoding='utf-8').write(content)
-print('done')
